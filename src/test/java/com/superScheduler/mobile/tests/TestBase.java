9ecd70d9a4e310127c898ea8578ef677446fe33e
varuwa <varuwa@gmail.com>
2020-05-21 17:46:35 +0300

    Added SuperScheduler project

---
+++ src/test/java/com/superScheduler/mobile/tests/TestBase.java
@@ -0,0 +1,42 @@
+package com.superScheduler.mobile.tests;
+
+import com.superScheduler.mobile.manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class TestBase {
+
+    static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeSuite
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters: " + Arrays.asList(p));
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        app.stop();
+    }
+
+}
